9e9d6230201cade2a31695a152af95317fe256d9
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-09 23:41:09 +0530

    basefile added

--- src/main/java/com/AlphaStore/Mail/MailApplication.kt
+++ src/main/java/com/AlphaStore/Mail/MailApplication.kt
@@ -1,17 +1,15 @@
-package com.AlphaStore.Mail;
+package com.AlphaStore.Mail
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAsync
-public class MailApplication {
+class MailApplication
 
-	public static void main(String[] args) {
-		SpringApplication.run(MailApplication.class, args);
-	}
-
+fun main(args: Array<String>) {
+    SpringApplication.run(MailApplication::class.java, *args)
 }
