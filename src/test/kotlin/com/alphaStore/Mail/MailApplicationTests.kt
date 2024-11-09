3b3782709ccda0f90f444e70d8c6f85109cc0493
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 00:32:40 +0530

    update

---
+++ src/test/kotlin/com/alphaStore/Mail/MailApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alphaStore.Mail
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MailApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
