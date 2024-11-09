99d557d57cc0292b0723c42eebd7ef459a099d34
AlphaTanmoy <dtanmoy169@gmail.com>
2024-11-10 00:54:44 +0530

    update

--- src/main/kotlin/com/alphaStore/Mail/MailApplication.kt
+++ src/main/kotlin/com/alphaStore/Mail/MailApplication.kt
@@ -2,8 +2,12 @@ package com.alphaStore.Mail
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableAsync
 class MailApplication
 
 fun main(args: Array<String>) {
